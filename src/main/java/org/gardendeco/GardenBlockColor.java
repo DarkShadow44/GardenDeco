8c02890a562a97362a852398fe14b84bf41bded3
Fabian Maurer <dark.shadow4@web.de>
2022-06-21 19:35:37 +0200

    For broken blockentities render default color

--- src/main/java/org/gardendeco/GardenBlockColor.java
+++ src/main/java/org/gardendeco/GardenBlockColor.java
@@ -14,12 +14,23 @@ public class GardenBlockColor implements BlockColor {
 	@Override
 	public int getColor(BlockState state, BlockAndTintGetter blockAndTintGetter, BlockPos pos, int unk) {
 		int color = -1;
+		ColorType type = ((IMimicBlock) state.getBlock()).getColorType();
 		EntityBlockMimic entity = (EntityBlockMimic) blockAndTintGetter.getBlockEntity(pos);
 		if (entity != null) {
-			ColorType type = ((IMimicBlock) state.getBlock()).getColorType();
 			color = MimicHandler.getBiomeColor(entity.getBiomeKey(), type);
 		}
 
-		return color != -1 ? color : BiomeColors.getAverageFoliageColor(blockAndTintGetter, pos);
+		if (color == -1) {
+			switch (type) {
+			case GRASS:
+				return BiomeColors.getAverageGrassColor(blockAndTintGetter, pos);
+			case FOLIAGE:
+				return BiomeColors.getAverageFoliageColor(blockAndTintGetter, pos);
+			case WATER:
+				return BiomeColors.getAverageWaterColor(blockAndTintGetter, pos);
+			}
+		}
+
+		return color;
 	}
 }
